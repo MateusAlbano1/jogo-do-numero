12bdca3364d4b8a6b671bfdfd8825c74fdfa9f23
MateusAlbano1 <mateus.philippus@escola.pr.gov.br>
2025-08-21 12:05:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let numeroAleatorio = Math.floor(Math.ranoom() *100) + 1;
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
-const bsixoOuAlto = document.querySelector('.baixoOuAlto');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
 let contagemPalpites = 1;
@@ -13,39 +13,38 @@ function verificarPalpite() {
         palpites.textContent = "Palpites anteriores: ";
     }
 
-palpites.textContent += palpiteUsuario + " ";
+    palpites.textContent += palpiteUsuario + " ";
 
-if (palpiteUsuario === numeroAleatorio) {
-  ultimoResultado.textContent = "Parabéns! Você é Ninja!";
-  ultimoResultado.style.backgroundColor = "green";
-  baixoAouAlto.textContent = "";
-  finalizarJogo();
-} else if (contagemPalpites === 10) {
-  ultimoResultado.textcontent = "VOCÊ É BURRO!!";
-  baixoOuAlto.textcontent = "";
-  finalizarJogo();
-} else {
-  ultimoResultado.textContent = "ERRADO";
-  ultimoResultado.style.backgroundColor = "red";
-  if (palpiteUsuario < numeroAleatorio) {
-    baixoOuAlto.textContent = "O último palpite foi muito baixo";
-  } else if (palpiteUsuario > numeroAleatorio) {
-    baixoOuAlto.textContent = "O último palpite foi muito alto";
-  }
-}
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = "Parabéns! Você é Ninja!";
+        ultimoResultado.style.backgroundColor = "green";
+        baixoOuAlto.textContent = "";
+        finalizarJogo();
+    } else if (contagemPalpites === 10) {
+        ultimoResultado.textContent = "VOCÊ É BURRO!!";
+        baixoOuAlto.textContent = "";
+        finalizarJogo();
+    } else {
+        ultimoResultado.textContent = "ERRADO";
+        ultimoResultado.style.backgroundColor = "red";
+        if (palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = "O último palpite foi muito baixo";
+        } else if (palpiteUsuario > numeroAleatorio) {
+            baixoOuAlto.textContent = "O último palpite foi muito alto";
+        }
+    }
 
-contagemPalpites++;
-campoPalpite.value = "";
-campoPalpite.focus();
+    contagemPalpites++;
+    campoPalpite.value = "";
+    campoPalpite.focus();
 }
 
 envioPalpite.addEventListener('click', verificarPalpite);
 
-function finalizarJogo () {
+function finalizarJogo() {
     campoPalpite.disabled = true;
     envioPalpite.disabled = true;
     botaoReiniciar = document.createElement('button');
-    document.body.appendChild(botaoReiniciar);
     botaoReiniciar.textContent = 'Reiniciar Jogo';
     botaoReiniciar.classList.add('botaoReiniciar');
     document.body.appendChild(botaoReiniciar);
@@ -54,16 +53,16 @@ function finalizarJogo () {
 
 function reiniciarJogo() {
     contagemPalpites = 1;
-    const paragrafosReiniciar = document. querySelectorAll('.paragrafosResultado p');
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
     for (const paragrafoReiniciar of paragrafosReiniciar) {
-        paragrafoReiniciar.textcontent = "";
+        paragrafoReiniciar.textContent = "";
     }
 
-botaoReiniciar.parentNode.removeChild(botaoReiniciar);
-campoPalpite.disabled = false;
-envioPalpite.disabled = false;
-campoPalpite.value = "";
-campoPalpite.focus();
-ultimoResultado.style.backgroundColor = 'white';
-numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+    campoPalpite.disabled = false;
+    envioPalpite.disabled = false;
+    campoPalpite.value = "";
+    campoPalpite.focus();
+    ultimoResultado.style.backgroundColor = 'white';
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 }
